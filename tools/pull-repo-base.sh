35d5a11942fc17d065466f186d307451286a840a
31.115dev_machine <wnpllr@gmail.com>
2021-05-13 14:15:23 +0800

    proxy pull repo

--- tools/pull-repo-base.sh
+++ tools/pull-repo-base.sh
@@ -7,9 +7,9 @@ LOCAL_WORKSPACE=$2
 if [ -z $REMOTE_REPO -o -z $LOCAL_WORKSPACE ]; then
     echo "invalid call pull-repo.sh '$REMOTE_REPO' '$LOCAL_WORKSPACE'"
 elif [ ! -d $LOCAL_WORKSPACE ]; then
-    git clone $REMOTE_REPO $LOCAL_WORKSPACE
+    git -c http.proxy=socks5://127.0.0.1:9977 clone $REMOTE_REPO $LOCAL_WORKSPACE
 else
     cd $LOCAL_WORKSPACE
-    git fetch --all --tags
+    git -c http.proxy=socks5://127.0.0.1:9977 fetch --all --tags
     cd -
 fi
